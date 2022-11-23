6dc4b033494e7d3c5d08d586f08b31d29e5a09f7
seyoung <seyoung@windows>
2022-11-23 20:54:45 +0900

    update readme

--- README.sh
+++ README.sh
@@ -1,8 +1,8 @@
 # build base
-docker build --tag seyoungsong/neko:base --platform linux/arm64 --file .docker/base/Dockerfile.arm64 .
+docker build --tag seyoungsong/neko:base --platform linux/arm64 --file Dockerfile.base neko
 
 # build chromium
-docker build --tag seyoungsong/neko:chromium --platform linux/arm64 --build-arg="BASE_IMAGE=seyoungsong/neko:base" --file .docker/chromium/Dockerfile.arm .docker/chromium
+docker build --tag seyoungsong/neko:chromium --platform linux/arm64 --build-arg="BASE_IMAGE=ghcr.io/seyoungsong/neko:base" --file neko/.docker/chromium/Dockerfile.arm neko/.docker/chromium
 
 # test chromium
 docker run -it --rm seyoungsong/neko:chromium --platform linux/arm64 /bin/bash
